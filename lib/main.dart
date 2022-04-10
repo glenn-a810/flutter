faf47e28be266b6205c026fcece526334acedeed
Glenn <a810.glenn@gmail.com>
2022-04-10 21:37:01 +0900

    목록 형태로 보이기 ogu ver.1

--- lib/main.dart
+++ lib/main.dart
@@ -11,13 +11,24 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(title: const Text('오구월드'),),
-        body: Align(
-          alignment: Alignment.centerLeft,
-          child: Container(
-            width: double.infinity, height: 200, color: Colors.blue,
-          ),
+        appBar: AppBar(
+          title: const Text('오구월드'),
+          // backgroundColor: const Color(0xffffffff),
+          actions: const [
+            Icon(Icons.search),
+            Icon(Icons.apps),
+            Icon(Icons.notifications),
+          ],
         ),
+        body: ListView(
+          children: <Widget>[
+            ListTile(
+              title: const Text('캐논 DSLR 100D (단렌즈, 충전기 16기가SD 포함)'),
+              subtitle: const Text('성동구 청담동 끌올 10분 전'),
+              leading: Image.asset('assets/ogu.gif'),
+            )
+          ]
+        )
       ),
     );
   }
