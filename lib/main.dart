6a98d0afc80739dfb95ef21667b1ce5288e50d59
Glenn <a810.glenn@gmail.com>
2022-04-11 22:25:38 +0900

    목록 형태로 보이기 ogu ver.2

--- lib/main.dart
+++ lib/main.dart
@@ -20,14 +20,41 @@ class MyApp extends StatelessWidget {
             Icon(Icons.notifications),
           ],
         ),
-        body: ListView(
-          children: <Widget>[
-            ListTile(
-              title: const Text('캐논 DSLR 100D (단렌즈, 충전기 16기가SD 포함)'),
-              subtitle: const Text('성동구 청담동 끌올 10분 전'),
-              leading: Image.asset('assets/ogu.gif'),
-            )
-          ]
+        // body: ListView(
+        //   children: <Widget>[
+        //     ListTile(
+        //       title: const Text('캐논 DSLR 100D (단렌즈, 충전기 16기가SD 포함)'),
+        //       subtitle: const Text('성동구 청담동 끌올 10분 전'),
+        //       leading: Image.asset('assets/ogu.gif'),
+        //     )
+        //   ]
+        // )
+        body: Container(
+          height: 150,
+          padding: EdgeInsets.all(10),
+          child: Row(
+            children: [
+              Image.asset('assets/ogu.gif', width: 70,),
+              SizedBox(
+                width: 300,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Text('캐논 DSLR 100D (단렌즈,충전기 16기가 SD포함)', style: TextStyle(fontWeight: FontWeight.bold),),
+                    const Text('성동구 청담동 끌올 10분 전'),
+                    const Text('680,000원'),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: const [
+                        Icon(Icons.favorite),
+                        Text('4'),
+                      ],
+                    )
+                  ],
+                ),
+              )
+            ],
+          ),
         )
       ),
     );
