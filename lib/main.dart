47c2dcdb71644094ac40080d6eef89d7ce21a2b7
Glenn <a810.glenn@gmail.com>
2022-04-13 21:29:16 +0900

    커스텀위젯 + ListView에서 좀 더 쉬운 문제풀이 + floatingActionButton 사용

--- lib/main.dart
+++ lib/main.dart
@@ -1,32 +1,48 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  MyApp({Key? key}) : super(key: key);
+
+  var a = 1;
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        floatingActionButton: FloatingActionButton(
+          child: Text(a.toString()),
+          onPressed: (){
+            a++;
+          },
+        ),
         appBar: AppBar(),
-        body: ListView(
-          children: const [
-            ListTile(
+        body: ListView.builder(
+          itemCount: 3, // 몇 번 반복할지
+          itemBuilder: (context,i){ // 첫번째 파라메터 context, 두번째 파라메터 반복때마다 증가되는 정수 i++같은거
+            print(i); // 콘솔창에 나타남
+            return const ListTile(
               leading: Icon(Icons.account_circle, size: 40,),
               title: Text('루이'),
-            ),
-            ListTile(
-              leading: Icon(Icons.account_circle, size: 40,),
-              title: Text('오드'),
-            ),
-            ListTile(
-              leading: Icon(Icons.account_circle, size: 40,),
-              title: Text('하루'),
-            ),
-          ],
+            );
+          },
+          // children: const [
+          //   ListTile(
+          //     leading: Icon(Icons.account_circle, size: 40,),
+          //     title: Text('루이'),
+          //   ),
+          //   ListTile(
+          //     leading: Icon(Icons.account_circle, size: 40,),
+          //     title: Text('오드'),
+          //   ),
+          //   ListTile(
+          //     leading: Icon(Icons.account_circle, size: 40,),
+          //     title: Text('하루'),
+          //   ),
+          // ],
           // children: [
           //   Row(
           //     children: const [
