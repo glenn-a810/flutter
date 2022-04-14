91779cfce8d6f3799c6b1ce2be2f039d0a4deef3
Glenn <a810.glenn@gmail.com>
2022-04-14 16:27:00 +0900

    StatefulWidget 사용법, state 활용 문제

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,7 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   var a = 1; // StatefulWidget으로 바꾸면 state로 인식
   var name = ['루이','오드','하루'];
+  var like = [0,0,0];
 
   @override
   Widget build(BuildContext context) {
@@ -32,10 +33,20 @@ class _MyAppState extends State<MyApp> {
         body: ListView.builder(
           itemCount: 3, // 몇 번 반복할지
           itemBuilder: (context,i){ // 첫번째 파라메터 context, 두번째 파라메터 반복때마다 증가되는 정수 i++같은거
-            print(i); // 콘솔창에 나타남
+            // print(i); // 콘솔창에 나타남
             return ListTile(
-              leading: const Icon(Icons.account_circle, size: 40,),
+              // leading: const Icon(Icons.account_circle, size: 40,),
+              leading: Text(like[i].toString()),
               title: Text(name[i]),
+              trailing: RaisedButton(
+                color: Colors.blue,
+                onPressed: (){
+                  setState(() {
+                    like[i]++;
+                  });
+                },
+                child: Text('좋아요',style: TextStyle(color: Colors.white),),
+              ),
             );
           },
           // children: const [
