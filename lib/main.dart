314ef02f10d9a180544f33eb40798f2d96f31838
Glenn <a810.glenn@gmail.com>
2022-04-14 16:12:56 +0900

    StatefulWidget 사용법, state 활용

--- lib/main.dart
+++ lib/main.dart
@@ -4,10 +4,16 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   MyApp({Key? key}) : super(key: key);
 
-  var a = 1;
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  var a = 1; // StatefulWidget으로 바꾸면 state로 인식
+  var name = ['루이','오드','하루'];
 
   @override
   Widget build(BuildContext context) {
@@ -16,17 +22,20 @@ class MyApp extends StatelessWidget {
         floatingActionButton: FloatingActionButton(
           child: Text(a.toString()),
           onPressed: (){
-            a++;
+            // a++;
+            setState(() {
+              a++;
+            });
           },
         ),
-        appBar: AppBar(),
+        appBar: AppBar(title: const Text('오구월드'),),
         body: ListView.builder(
           itemCount: 3, // 몇 번 반복할지
           itemBuilder: (context,i){ // 첫번째 파라메터 context, 두번째 파라메터 반복때마다 증가되는 정수 i++같은거
             print(i); // 콘솔창에 나타남
-            return const ListTile(
-              leading: Icon(Icons.account_circle, size: 40,),
-              title: Text('루이'),
+            return ListTile(
+              leading: const Icon(Icons.account_circle, size: 40,),
+              title: Text(name[i]),
             );
           },
           // children: const [
